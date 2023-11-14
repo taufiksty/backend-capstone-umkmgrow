5b0a39bd8763b29812d733a3c30e5f04fbd9d1d4
taufiksty <taufik.satya09@gmail.com>
2023-11-14 20:38:24 +0700

    feat: add common middlewares

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const { authRouter } = require('./routes');
+const { errorHandler, notFound } = require('./middlewares');
 
 const app = express();
 
@@ -9,5 +11,10 @@ app.use(express.json());
 app.get('/', (req, res) => {
 	res.send('Welcome to UMKM Grow API');
 });
+
+app.use('/api/auth', authRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
---
+++ src/middlewares/index.js
@@ -0,0 +1,5 @@
+const asyncWrapper = require('./async');
+const errorHandler = require('./error-handler');
+const notFound = require('./not-found');
+
+module.exports = { asyncWrapper, errorHandler, notFound };
