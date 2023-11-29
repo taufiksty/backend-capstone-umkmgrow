d72a63a6cf4098ba93549566bcd7576bca6e5322
taufiksty <taufik.satya09@gmail.com>
2023-11-29 15:04:02 +0700

    modify data courses

--- src/config/data/courses.js
+++ src/config/data/courses.js
@@ -1,60 +1,60 @@
 const COURSES = [
-  {
-    id: `course-rUbJU5kzAdyK9CM7`,
-    course_name: "Fundamental Pemasaran Digital untuk UMKM",
-    course_description: "Pelajari dasar-dasar pemasaran digital untuk UMKM.",
-    course_about:
-      "Pelajari dasar-dasar pemasaran digital untuk UMKM, termasuk strategi pemasaran melalui media sosial, SEO (Search Engine Optimization), dan penggunaan iklan online. Course ini akan membantu UMKM meningkatkan visibilitas online mereka dan menjangkau pasar yang lebih luas.",
-    terms:
-      '["Pemahaman dasar tentang operasi bisnis dan target pasar UMKM.","Kemampuan dasar dalam penggunaan komputer dan internet."]',
-    for_who: "Pemula hingga Menengah",
-    materials:
-      '["Pendahuluan Pemasaran Digital","Media Sosial dan Pemasaran","SEO (Search Engine Optimization) untuk UMKM","Iklan Online untuk UMKM","Analisis dan Pengukuran Kinerja","Studi Kasus dan Best Practices"]',
-    certification: true,
-    instructor_id: "user-KO1U98myYpTIPSKh",
-    category_path: '["grow digital","grow your market"]',
-    price: 0,
-    image_course:
-      "http://localhost:3000/images/course/fundamental-digital-marketing-for-small-businesses-upscaled.png",
-  },
-  {
-    id: `course-Mzyu0FwLrSeGSMcJ`,
-    course_name: "Strategi Pertumbuhan Bisnis E-Commerce",
-    course_description:
-      "Fokus pada strategi e-commerce untuk mengembangkan bisnis UMKM.",
-    course_about:
-      "Fokus pada strategi e-commerce untuk mengembangkan bisnis UMKM. Materi kursus meliputi pemilihan platform e-commerce, manajemen inventaris online, strategi harga, dan cara meningkatkan penjualan melalui toko online. Course ini dirancang untuk membantu UMKM memanfaatkan potensi e-commerce untuk pertumbuhan bisnis mereka.",
-    terms:
-      '["Pengetahuan dasar tentang operasi bisnis UMKM.","Akses ke internet dan pemahaman dasar tentang teknologi."]',
-    for_who: "Pemula hingga Menengah",
-    materials:
-      '["Pengenalan E-commerce","Memilih Platform E-commerce yang Tepat","Manajemen Inventaris Online","Strategi Harga dan Penawaran Khusus","Optimasi Konversi dan Pengalaman Pelanggan","Pemasaran untuk Toko Online","Analisis Kinerja E-commerce"]',
-    certification: true,
-    instructor_id: "user-KO1U98myYpTIPSKh",
-    category_path: '["grow digital","grow your business"]',
-    price: 0,
-    image_course:
-      "http://localhost:3000/images/course/e-commerce-strategies-for-business-growth-upscaled.png",
-  },
-  {
-    id: `course-oVIaqcJfQUPDi75-`,
-    course_name: "Branding dan Eksistensi Pasar untuk UMKM",
-    course_description:
-      "Explore konsep dasar brand identity dan bagaimana UMKM dapat membangun citra merek yang kuat.",
-    course_about:
-      "Explore konsep dasar brand identity dan bagaimana UMKM dapat membangun citra merek yang kuat. Course ini juga membahas strategi penempatan pasar untuk membedakan bisnis dari pesaing. Dengan memahami prinsip-prinsip ini, UMKM dapat mengembangkan dan memperluas pangsa pasar mereka.",
-    terms:
-      '["Pemahaman dasar tentang jenis bisnis UMKM dan target pasar.","Minat dalam mengembangkan citra merek yang kuat."]',
-    for_who: "Pemula hingga Menengah",
-    materials:
-      '["Pendahuluan Branding untuk UMKM","Strategi Pembentukan Identitas Merek","Segmentasi Pasar dan Penempatan Merek","Komunikasi Merek yang Efektif","Manajemen Reputasi Merek","Pemahaman Pasar dan Persaingan","Studi Kasus: Merek Sukses dan Kegagalan"]',
-    certification: true,
-    instructor_id: "user-KO1U98myYpTIPSKh",
-    category_path: '["grow your business","grow your market"]',
-    price: 0,
-    image_course:
-      "http://localhost:3000/images/course/branding-and-market-positioning-for-small-business-upscaled.png",
-  },
+	{
+		id: `course-rUbJU5kzAdyK9CM7`,
+		course_name: 'Fundamental Pemasaran Digital untuk UMKM',
+		course_description: 'Pelajari dasar-dasar pemasaran digital untuk UMKM.',
+		course_about:
+			'Pelajari dasar-dasar pemasaran digital untuk UMKM, termasuk strategi pemasaran melalui media sosial, SEO (Search Engine Optimization), dan penggunaan iklan online. Course ini akan membantu UMKM meningkatkan visibilitas online mereka dan menjangkau pasar yang lebih luas.',
+		terms:
+			'["Pemahaman dasar tentang operasi bisnis dan target pasar UMKM.","Kemampuan dasar dalam penggunaan komputer dan internet."]',
+		for_who: 'Pemula hingga Menengah',
+		materials:
+			'["Pendahuluan Pemasaran Digital","Media Sosial dan Pemasaran","SEO (Search Engine Optimization) untuk UMKM","Iklan Online untuk UMKM","Analisis dan Pengukuran Kinerja","Studi Kasus dan Best Practices"]',
+		certification: true,
+		instructor_id: 'user-92Qosd_cZDqIfUmi',
+		category_path: '["grow digital","grow your market"]',
+		price: 0,
+		image_course:
+			'http://localhost:3000/images/course/fundamental-digital-marketing-for-small-businesses-upscaled.png',
+	},
+	{
+		id: `course-Mzyu0FwLrSeGSMcJ`,
+		course_name: 'Strategi Pertumbuhan Bisnis E-Commerce',
+		course_description:
+			'Fokus pada strategi e-commerce untuk mengembangkan bisnis UMKM.',
+		course_about:
+			'Fokus pada strategi e-commerce untuk mengembangkan bisnis UMKM. Materi kursus meliputi pemilihan platform e-commerce, manajemen inventaris online, strategi harga, dan cara meningkatkan penjualan melalui toko online. Course ini dirancang untuk membantu UMKM memanfaatkan potensi e-commerce untuk pertumbuhan bisnis mereka.',
+		terms:
+			'["Pengetahuan dasar tentang operasi bisnis UMKM.","Akses ke internet dan pemahaman dasar tentang teknologi."]',
+		for_who: 'Pemula hingga Menengah',
+		materials:
+			'["Pengenalan E-commerce","Memilih Platform E-commerce yang Tepat","Manajemen Inventaris Online","Strategi Harga dan Penawaran Khusus","Optimasi Konversi dan Pengalaman Pelanggan","Pemasaran untuk Toko Online","Analisis Kinerja E-commerce"]',
+		certification: true,
+		instructor_id: 'user-92Qosd_cZDqIfUmi',
+		category_path: '["grow digital","grow your business"]',
+		price: 0,
+		image_course:
+			'http://localhost:3000/images/course/e-commerce-strategies-for-business-growth-upscaled.png',
+	},
+	{
+		id: `course-oVIaqcJfQUPDi75-`,
+		course_name: 'Branding dan Eksistensi Pasar untuk UMKM',
+		course_description:
+			'Explore konsep dasar brand identity dan bagaimana UMKM dapat membangun citra merek yang kuat.',
+		course_about:
+			'Explore konsep dasar brand identity dan bagaimana UMKM dapat membangun citra merek yang kuat. Course ini juga membahas strategi penempatan pasar untuk membedakan bisnis dari pesaing. Dengan memahami prinsip-prinsip ini, UMKM dapat mengembangkan dan memperluas pangsa pasar mereka.',
+		terms:
+			'["Pemahaman dasar tentang jenis bisnis UMKM dan target pasar.","Minat dalam mengembangkan citra merek yang kuat."]',
+		for_who: 'Pemula hingga Menengah',
+		materials:
+			'["Pendahuluan Branding untuk UMKM","Strategi Pembentukan Identitas Merek","Segmentasi Pasar dan Penempatan Merek","Komunikasi Merek yang Efektif","Manajemen Reputasi Merek","Pemahaman Pasar dan Persaingan","Studi Kasus: Merek Sukses dan Kegagalan"]',
+		certification: true,
+		instructor_id: 'user-92Qosd_cZDqIfUmi',
+		category_path: '["grow your business","grow your market"]',
+		price: 0,
+		image_course:
+			'http://localhost:3000/images/course/branding-and-market-positioning-for-small-business-upscaled.png',
+	},
 ];
 
 module.exports = COURSES;
