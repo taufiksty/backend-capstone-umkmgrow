9364e2fe94a141e668ede9fc08eee08a491e3410
taufiksty <taufik.satya09@gmail.com>
2024-05-29 15:42:38 +0700

    fix: Trying to solve 3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require('./app');
-const config = require('./config');
-const sequelize = require('./config/database');
+const app = require('./src/app');
+const config = require('./src/config');
+const sequelize = require('./src/config/database');
 
 const HOST =
 	config.app.env === 'production' ? config.app.hostProd : config.app.hostDev;
